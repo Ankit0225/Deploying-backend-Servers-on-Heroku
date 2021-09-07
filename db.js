4507f64970b9edff5300538aa49dc3d95060b528
Ankit <ankitmaurya0225@gmail.com>
2021-09-07 21:02:59 +0530

    something tested and added

---
+++ db.js
@@ -0,0 +1,27 @@
+const sequelize = require('sequelize')
+
+const db = new sequelize({
+    dialect: 'sqlite',
+    storage: __dirname + '/tasks.db'
+})
+
+const Tasks = db.define('task', {
+    id: {
+        type: sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    done: {
+        type: sequelize.DataTypes.BOOLEAN,
+        defaultValue: false,
+    }
+
+}) 
+
+module.exports = {
+    db, Tasks
+}
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+
+const {db, Tasks} = require('./db')
 const path = require('path')
 
 const PORT = process.env.PORT || 5555
@@ -6,11 +8,25 @@ const app = express()
 
 app.use('/public' , express.static(path.join(__dirname , 'public')))
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (req,res)=> 
   res.send(`Hello from Backend \n
   Add /public to know more about project`)
 )
+app.get('/tasks', async (req,res) => {
+  res.send(await Tasks.findAll())
+})
 
-app.listen(PORT,()=> {
-    console.log(`Server started at http://localhost:${PORT}`);
+
+app.post('/tasks', async(req,res) => {
+  res.send(await Tasks.create(req.body))
+})
+
+db.sync()
+.then(() => {
+  app.listen(PORT,()=> {
+      console.log(`Server started at http://localhost:${PORT}`);
+  })
 })
